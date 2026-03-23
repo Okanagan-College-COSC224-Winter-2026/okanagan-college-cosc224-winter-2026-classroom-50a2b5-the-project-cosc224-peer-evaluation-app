34efbc90620bba258bd0e8ba2808d82940bf1a67
Sully Williams <sullytommy15@gmail.com>
2026-03-22 23:09:35 -0700

    finished front end for us27. should be complete now

--- frontend/src/pages/Group.tsx
+++ frontend/src/pages/Group.tsx
@@ -18,7 +18,7 @@ import { useParams } from "react-router-dom";
 import "./Group.css";
 import TabNavigation from "../components/TabNavigation";
 import StatusMessage from "../components/StatusMessage";
-import { hasRole } from "../util/login";
+import { hasRole, isTeacher } from "../util/login";
 import Textbox from "../components/Textbox";
 
 function fisherYates<T>(array: T[]): T[] {
@@ -377,7 +377,11 @@ export default function Group() {
             <div>
               
             </div>
-        {canManageGroups ? (
+
+            <div>
+              
+            </div>
+        {isTeacher() ? (
           <>
             <div className="assignmentTables">
               <table className="table">
