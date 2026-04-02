5b0e74a41b5bbc445df9b5d9afd4b2e5446f3e0e
Simon Velez <simonveca8@gmail.com>
2026-04-01 21:20:28 -0700

    feat: responsive design (mobile + tablet support)

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,35 +1,65 @@
+import { useState } from 'react'
 import { logout } from '../util/login'
 import './Sidebar.css'
 
 export default function Sidebar() {
+  const [isOpen, setIsOpen] = useState(false)
+
   // Check which page we are on
   const location = window.location.pathname
 
+  const toggleSidebar = () => setIsOpen(!isOpen)
+  const closeSidebar = () => setIsOpen(false)
+
   return (
-    <div className="Sidebar">
-      <div className="SidebarLogo">
-        <img src="/oc_logo.png" alt="OC Logo" />
+    <>
+      {/* Hamburger button (mobile only) */}
+      <button className="hamburger-btn" onClick={toggleSidebar}>
+        ☰
+      </button>
+
+      <div className={`Sidebar ${isOpen ? 'open' : ''}`}>
+        <div className="SidebarLogo">
+          <img src="/oc_logo.png" alt="OC Logo" />
+        </div>
+
+        <div className="SidebarTop">
+          <SidebarRow
+            onClick={() => {
+              logout()
+              closeSidebar()
+            }}
+            href="#"
+            selected={false}
+          >
+            Logout
+          </SidebarRow>
+
+          <SidebarRow
+            selected={location === '/home'}
+            href="/home"
+            onClick={closeSidebar}
+          >
+            Home
+          </SidebarRow>
+
+          {/* TODO: make this ID match who is logged in */}
+          <SidebarRow
+            selected={location.includes('/profile')}
+            href="/profile/1"
+            onClick={closeSidebar}
+          >
+            My Info
+          </SidebarRow>
+        </div>
       </div>
 
-      <div className="SidebarTop">
-        <SidebarRow
-          onClick={() => logout()}
-          href='#'
-          selected={false}
-        >
-          Logout
-        </SidebarRow>
-
-        <SidebarRow selected={location === '/home'} href="/home">
-          Home
-        </SidebarRow>
-        
-        { /* TODO: make this ID match who is logged in */ }
-        <SidebarRow selected={location.includes('/profile')} href="/profile/1">
-          My Info
-        </SidebarRow>
-      </div>
-    </div>
+      {/* overlay */}
+      <div
+        className="sidebar-overlay"
+        onClick={closeSidebar}
+      />
+    </>
   )
 }
 
@@ -42,7 +72,10 @@ interface SidebarRowProps {
 
 function SidebarRow(props: SidebarRowProps) {
   return (
-    <div className={`SidebarRow ${props.selected ? 'selected' : ''}`} onClick={props.onClick}>
+    <div
+      className={`SidebarRow ${props.selected ? 'selected' : ''}`}
+      onClick={props.onClick}
+    >
       <a href={props.selected ? '#' : props.href}>{props.children}</a>
     </div>
   )
